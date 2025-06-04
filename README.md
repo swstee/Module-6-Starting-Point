c485bc47ffd6c27c6244aee0cae75accf0ae2326
Steven Wong <swstee@gmail.com>
2025-06-05 00:24:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Portfolio Site Build
+
+Module 6 attempts to pull together all the previous lessons into a single portfolio site.
---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+	title: "Katie Haus Portfolio Site",
+	description:
+		"Welcome to my portfolio site! I'm a self-taught designer and developer obsessed with quality.",
+};
