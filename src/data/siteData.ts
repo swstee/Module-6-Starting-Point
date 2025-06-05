5feba18ead1156e3d5e379e4c4248242cdc0bc73
Steven Wong <swstee@gmail.com>
2025-06-05 23:12:05 +0800

    adding hero heading nav demo images

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,5 +1,5 @@
 export const baseData = {
-	title: "Katie Haus Portfolio Site",
+	title: "Steven Portfolio Site",
 	description:
 		"Welcome to my portfolio site! I'm a self-taught designer and developer obsessed with quality.",
 };
